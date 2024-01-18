ceba802f46e075f38d034d6b4275a4cc0d650915
MineFact <minefact.minecraft@gmail.com>
2024-01-18 17:29:00 -0600

    fixed already up to date

--- updater.py
+++ updater.py
@@ -25,34 +25,48 @@ def get_version(filename):
     return filename.split('Obsidian.')[1].split('.exe')[0]
 
 def is_latest_release(latest_release, local_exe_path):
+    if local_exe_path is None:
+        return False
+    if not os.path.exists(local_exe_path):
+        return False
+
     latest_release_version = get_version(latest_release)
     local_exe_version = get_version(os.path.basename(local_exe_path))
 
-    return latest_release_version != local_exe_version
+    return latest_release_version == local_exe_version
 
 def download_latest_obsidian(releases_url, local_exe_path, directory):
     print("Fetching the latest release from GitHub...")
     response = requests.get(releases_url)
     response.raise_for_status()
     latest_release = response.json()[0]
+    latest_release_filename = get_versioned_filename(latest_release)
     print(f"Latest release: {latest_release['tag_name']}")
+    print(f"Latest release filename: {latest_release_filename}")
 
     # Check if the latest release is the same as the local executable
-    if is_latest_release(latest_release, local_exe_path):
+    if is_latest_release(latest_release_filename, local_exe_path):
         print("The Obsidian executable is already up to date.")
-        return
+        return True
 
     download_url = latest_release['assets'][0]['browser_download_url']
     print(f"Download URL: {download_url}")
 
-    new_filename = get_versioned_filename(latest_release)
-    download_path = os.path.join(directory, new_filename)
-    print(f"Downloading and saving as: {new_filename}")
+    download_path = os.path.join(directory, latest_release_filename)
+    print(f"Downloading and saving as: {latest_release_filename}")
 
     response = requests.get(download_url)
     with open(download_path, "wb") as file:
         file.write(response.content)
 
+    return False
+
+def replace_latest_obsidian(releases_url, local_exe_path, directory):
+    is_latest_release = download_latest_obsidian(releases_url, local_exe_path, directory)
+
+    if is_latest_release:
+        return
+    
     try:
         print(f"Attempting to replace old executable: {local_exe_path}")
         if os.path.exists(local_exe_path):
@@ -73,6 +87,6 @@ else:
 obsidian_exe_path = find_obsidian_executable(directory)
 
 if obsidian_exe_path:
-    download_latest_obsidian(releases_url, obsidian_exe_path, directory)
+    replace_latest_obsidian(releases_url, obsidian_exe_path, directory)
 else:
-    print("Script did not find any Obsidian executable to update.")
+    download_latest_obsidian(releases_url, obsidian_exe_path, directory)
